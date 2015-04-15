afd5226e6c02cf2e218b9d53a67081f1f9adf9d1
kerbey <kerbeychevalier@verizon.net>
2015-04-15 00:55:36 -0400

    Update and rename BorderLayoutDemo.java to flowLayoutDemo.java

--- FlowLayout/src/flowLayoutDemo.java
+++ FlowLayout/src/flowLayoutDemo.java
@@ -1,6 +1,8 @@
+package borderlayoutdemo;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -8,41 +10,33 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-
-public class BorderLayoutDemo extends JFrame implements ActionListener  
+public class flowLayoutDemo  extends JFrame implements ActionListener
 {
-	public static BorderLayout Layout;
-	public static JFrame frame;
 	public static JButton button1, button2, button3, button4, button5, button6;
+	public static JFrame frame;
 	public static JPanel panel1, panel2;
-
-	public  BorderLayoutDemo()
+	public flowLayoutDemo()
 	{
 		panel1= new JPanel();
 		panel2= new JPanel();
-		frame = new JFrame(" BorderLayout ");
-		frame.setLayout(new BorderLayout());
-		frame.setSize(new Dimension(250,92));
-
-		panel1.setLayout(new FlowLayout());
-		panel2.setLayout(new FlowLayout());
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+		frame = new JFrame(" FlowLayout Demo ");
+		frame.setSize(new Dimension(500,90));//(width, height)
 		button1= new JButton("1");
 		button2= new JButton("2");
 		button3= new JButton("3");
 		button4= new JButton("4");
 		button5= new JButton("5");
 		button6= new JButton("6");
-		panel1.add(button4);
-		panel1.add(button5);
-		panel1.add(button6);
-		panel2.add(button1);
-		panel2.add(button2);
-		panel2.add(button3);
-		frame.add(panel1, BorderLayout.NORTH);
-		frame.add(panel2, BorderLayout.SOUTH);
-
+		frame.setLayout(new FlowLayout());
+		panel1.add(button1);//first 3 buttons added to the first panel and last 3 added to the second panel
+		panel1.add(button2);
+		panel1.add(button3);
+		panel2.add(button4);
+		panel2.add(button5);
+		panel2.add(button6);
+		frame.add(panel1);
+		frame.add(panel2);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		frame.setLocation(600,200);//(x,y) coordinates
 		button1.addActionListener(this);
@@ -104,8 +98,9 @@ public class BorderLayoutDemo extends JFrame implements ActionListener
 	{
 		if (e.getSource()==button1 ||e.getSource()==button2 || e.getSource()==button3
 				||e.getSource()==button4||e.getSource()==button5||e.getSource()==button6)
-		{//each time a button is pressed you close the window
+		{
 			System.exit(0);
 		}
 	}
 }
+
